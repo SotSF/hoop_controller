e214532410eaaf5da1e660415754d4130d22d20d
Kevin Reynolds <tossrock@gmail.com>
2017-05-13 01:26:37 -0700

    Add some more notes to the ol 2300 song

--- Song2300.h
+++ Song2300.h
@@ -15,7 +15,9 @@ static const chord score[] =
 	e_minor, Notes[e], Notes[a], Notes[b],
 	e_minor, Notes[e], Notes[b], Notes[b],
 	c_octave, Notes[c], Notes[C], c_octave,
-	Notes[c], Notes[cs], Notes[d], Notes[ds]
+	Notes[c], Notes[cs], Notes[d], Notes[ds],
+	Notes[C], Notes[D], Notes[Ds], Notes[F],
+	rest, rest, rest, rest
 };
 
 SongClass Song2300 = SongClass(BPM, sizeof(score)/sizeof(score[0]), score);
